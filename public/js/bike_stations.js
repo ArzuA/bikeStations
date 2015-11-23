fb53b9b255648cb1c5d42fce22cf2c7f10ea9473
Arzu Atilgan <arzuatilgan@yahoo.com>
2015-11-23 08:56:59 -0500

    Added search functionality and legend window on map

--- public/js/bike_stations.js
+++ public/js/bike_stations.js
@@ -1,19 +1,39 @@
-//Create the Google Map
 function initMap() {
+    var map = new google.maps.Map(document.getElementById('stations-map'), {
+        center: {lat: 43.653226, lng: -79.3831843},
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
 
-    var mapCanvas = document.getElementById('stations-map');
-    var mapOptions = {
-      center: new google.maps.LatLng(43.653226, -79.3831843),
-      zoom: 14,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU }
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(document.getElementById('legend'));
+
+    // Create legend for Markers
+    var legend = document.getElementById('legend');
+
+    var legendStyles= [
+        {name: 'Available Bike = 0', icon: "/images/cycling-wht.png"},
+        {name: 'Available Bike <= Available Docs', icon: "/images/cycling-ylw.png"},
+        {name: 'Available Bike > Available Docs', icon: "/images/cycling-orange.png"},
+        {name: 'Available Docs = 0', icon: "/images/cycling-red.png"}
+    ];
+
+    for (var i = 0; i < legendStyles.length; i++) {
+        var name = legendStyles[i].name;
+        var icon = legendStyles[i].icon;
+        var div = document.createElement('div');
+        div.innerHTML = '<img src="' + icon + '"> ' + name;
+        legend.appendChild(div);
     }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
 
-    stationManager(map);
+    stationManager(map, searchBox); 
 }
 
-function stationManager(map) {
+function stationManager(map, searchBox) {
 
     // Private:
 
@@ -25,6 +45,65 @@ function stationManager(map) {
     // Backbone template
     this.fTemplate = _.template(jQuery('#station-detail-template').html());
 
+    this.canSearch = function(searchBox) {
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+            searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+
+        // Listen for change in searcbox input area
+        $("#pac-input").off('focus.input').on('focus.input', function(e) {
+            $(this).val('');
+        });
+
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+
+        searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+              return;
+            }
+
+            // Clear out the old markers.
+            markers.forEach(function(marker) {
+              marker.setMap(null);
+            });
+
+            // For each place, get the icon, name and location.
+            var bounds = new google.maps.LatLngBounds();
+            places.forEach(function(place) {
+                var icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(90, 90),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+                // Create a marker for each place.
+                markers.push(new google.maps.Marker({
+                    map: map,
+                    icon: icon,
+                    title: place.name,
+                    position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }              
+            });
+            map.fitBounds(bounds);
+            var zoom = map.getZoom();
+            map.setZoom(zoom > 14 ? 14 : zoom);
+        });
+    }
+
     this.createOption = function(value, name) {
         var option = document.createElement('option');
         option.value = value;
@@ -32,30 +111,34 @@ function stationManager(map) {
         return option;
     }
 
-    this.createMarker = function(lat, lng, name) {
+    this.createMarker = function(station) {
         // Create a point with Google LatLng object to pass to marker
-        var point = new google.maps.LatLng(lat, lng);
+        var point = new google.maps.LatLng(station.latitude, station.longitude);
 
-        /*var iconBase = 'https://maps.google.com/mapfiles/kml/paddle/';
+        var iconBase = '../images/';
         var iconSuffix;
 
-        if (station.availableBikes > 0 && station.availableDocks > 0) {
-            iconSuffix = 'orange-circle.png';
+        if (station.availableBikes == 0 && station.availableDocks > 0) {
+            iconSuffix = 'cycling-wht.png';
+        } else if (station.availableBikes <= station.availableDocks && station.availableBikes > 0) {
+            iconSuffix = 'cycling-ylw.png';
+        } else if (station.availableBikes > station.availableDocks && station.availableDocks > 0) {
+            iconSuffix = 'cycling-orange.png';
         } else {
-            iconSuffix = 'orange-circle.png';
-        }*/
+            iconSuffix = 'cycling-red.png';
+        }
 
         // Create the Google Marker Point with the LatLng object
         var marker = new google.maps.Marker({
             position : point,
             map : map,
-            title : name,
-            /*icon : {
+            title : station.stationName,
+            icon : {
                 url: iconBase + iconSuffix,
                 origin: new google.maps.Point(0, 0),
                 //anchor: new google.maps.Point(parseFloat(25),parseFloat(25)),
-                scaledSize: new google.maps.Size(parseFloat(35),parseFloat(35))
-            }*/
+                scaledSize: new google.maps.Size(parseFloat(30),parseFloat(35))
+            }
         });
         return marker;
     }
@@ -128,8 +211,9 @@ function stationManager(map) {
                 $('#stations-list').append($(option));
 
                 if (statusValue != 'Not In Service'  ) {
-                    var marker = _self.createMarker(lat, lng, name);
+                    var marker = _self.createMarker(station);
                     var point = marker.position;
+
                     // Create a new instance of LatLngBounds to use for re-centering the map after all the stations are loaded
                     var bounds = new google.maps.LatLngBounds();
 
@@ -156,7 +240,9 @@ function stationManager(map) {
             alert(err);
         });
     }
+    _self.canSearch(searchBox);
     _self.getStations();
+
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
