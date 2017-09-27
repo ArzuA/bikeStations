e634576178d65d334ef2f39a246527c4a0422890
ArzuA <ArzuA@users.noreply.github.com>
2017-09-27 18:21:22 -0400

    live json link is updated

--- torontoBikes.js
+++ torontoBikes.js
@@ -1,7 +1,9 @@
 var jQuery = require('js-toolbox')._jQuery;
 
+//https://feeds.bikesharetoronto.com/stations/stations.json is no longer available
+
 var torontoBikes = function (req, res, next, callback){
-	jQuery.ajax({url:"http://www.bikesharetoronto.com/stations/json"})
+	jQuery.ajax({url:"https://tor.publicbikesystem.net/ube/stations"})
 	.done(function(data){
 		res.setHeader("Content-Type", "application/json");
 		res.end(data);
